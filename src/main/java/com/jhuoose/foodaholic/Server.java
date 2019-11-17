3900d01dd01e0a5d4e02f1eee45f64321973b202
Haoran Li <lihr04@outlook.com>
2019-11-17 18:36:44 -0500

    migrate from sqlite to postgresql
    Signed-off-by: Haoran Li <lihr04@outlook.com>

--- src/main/java/com/jhuoose/foodaholic/Server.java
+++ src/main/java/com/jhuoose/foodaholic/Server.java
@@ -11,22 +11,34 @@ import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class Server {
     public static void main(String[] args) throws SQLException {
-        var connection = DriverManager.getConnection("jdbc:sqlite:todoose.db");
-        var userRepository = new UserRepository(connection);
-        var userController = new UserController(userRepository);
-        Javalin.create(config -> { config.addStaticFiles("/public"); })
-        .events(event -> {
-            event.serverStopped(() -> { connection.close(); });
-        })
-        .routes(() -> {
-            path("users", () -> {
-                post(userController::register);
-                path("login", () -> {
-                    post(userController::login);
-                });
-            });
-        })
-//        .exception(ItemNotFoundException.class, (e, ctx) -> { ctx.status(404); })
-        .start(System.getenv("PORT") == null ? 4000 : Integer.parseInt(System.getenv("PORT")));
+//        var connection = DriverManager.getConnection("jdbc:sqlite:todoose.db");
+        try {
+            Class.forName("org.postgresql.Driver");
+            var connection = System.getenv("DATABASE_URL") == null ?
+                    DriverManager.getConnection("jdbc:postgresql://localhost:5432/foodaholic",
+                            "postgres", "postgres") :
+                    DriverManager.getConnection(System.getenv("DATABASE_URL"));
+            var userRepository = new UserRepository(connection);
+            var userController = new UserController(userRepository);
+            Javalin.create(config -> { config.addStaticFiles("/public"); })
+                    .events(event -> {
+                        event.serverStopped(() -> { connection.close(); });
+                    })
+                    .routes(() -> {
+                        path("users", () -> {
+                            post(userController::register);
+                            path("login", () -> {
+                                post(userController::login);
+                            });
+                        });
+                    })
+//                    .exception(ItemNotFoundException.class, (e, ctx) -> { ctx.status(404); })
+                    .start(System.getenv("PORT") == null ? 4000 : Integer.parseInt(System.getenv("PORT")));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+
     }
 }
